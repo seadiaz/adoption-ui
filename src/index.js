7a2f04f442b5fe5498a5e835ea27cd40e5e8acc9
Sebastian Diaz <sebastian.diaz@walmart.com>
2019-11-21 12:39:57 -0300

    feat[config]: add reducer and action for configuration. fix[github-actions]: remove variable for build which are moved to configuration section

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
 import adoptionReducer from './store/reducers/adoption'
 import toolsReducer from './store/reducers/tools'
 import monitorReducer from './store/reducers/monitor'
+import configReducer from './store/reducers/config'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
@@ -16,7 +17,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const reducer = combineReducers({
     adoption: adoptionReducer,
     tools: toolsReducer,
-    monitor: monitorReducer
+    monitor: monitorReducer,
+    config: configReducer
 })
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
